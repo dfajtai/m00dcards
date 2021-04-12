86c13c46bd57096cb081cb580f13b1dc027a62e4
dfajtai <daniel.fajtai@hotmail.com>
2021-04-12 08:58:38 +0200

    Update composer.json, composer.lock, and 1034 more files...

--- webpage/simple/init_session.php
+++ webpage/simple/init_session.php
@@ -1,5 +1,5 @@
 <?php
-function get_new_ID()
+function generateNewGame()
 {
     global $database;
     $new_id = uniqid("game_", true);
@@ -7,6 +7,11 @@ function get_new_ID()
     $database->insert("sessions", ["game_id" => $new_id, "indices" => ""]);
     return $new_id;
 }
+function hasGameId($gameID){
+    $dbGameID ;
+    
+}
+
 if (!htmlspecialchars($_GET["gameID"])) {
     #no gameID in url
     //echo "<script> console.log('no gameID in url') </script>";
@@ -31,7 +36,6 @@ if (!htmlspecialchars($_GET["gameID"])) {
     $gameID = htmlspecialchars($_GET["gameID"]);
     if ($database->count("sessions", ["game_id" => $gameID]) == 0) {
         #id unknown to database
-        //echo "<script> console.log('gameID stored in url unknown to database') </script>";
         $gameID = get_new_ID();
         $current_uri = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
         $current_uri = explode("?", $current_uri)[0];
@@ -39,5 +43,4 @@ if (!htmlspecialchars($_GET["gameID"])) {
         header("Location: " . $current_uri . "/?gameID=" . $gameID);
         exit;
     }
-    setcookie("gameID", $gameID,  time()+30*24*60*60);
 }
