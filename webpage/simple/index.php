5a9b97c970fdee499a290c538e05bffabb69291d
dfajtai <daniel.fajtai@hotmail.com>
2021-04-14 11:00:15 +0200

    bug fixes

--- webpage/simple/index.php
+++ webpage/simple/index.php
@@ -2,7 +2,7 @@
 $CARD_COUNT = 62; #number of cards in deck
 $init_state = range(1, $CARD_COUNT);
 $current_state = array();
-$state_counter = 0;
+$state_counter = 1;
 $selected_index = -1;
 session_start();
 require_once 'vendor/autoload.php';
@@ -17,18 +17,35 @@ include 'php/post_state.php';
 <!DOCTYPE html>
 <html lang="hu">
 
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/modernizr.min.js"></script>
+
 <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>m00dcards</title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
-        <script src="js/jquery-3.6.0.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/modernizr.min.js"></script>
         <link href="grid.css" rel="stylesheet">
+
+        <style>
+                .bd-placeholder-img {
+                        font-size: 1.125rem;
+                        text-anchor: middle;
+                        -webkit-user-select: none;
+                        -moz-user-select: none;
+                        user-select: none;
+                }
+
+                @media (min-width: 768px) {
+                        .bd-placeholder-img-lg {
+                                font-size: 3.5rem;
+                        }
+                }
+        </style>
 </head>
 
-<body class="py-4">
+<body>
         <script>
                 // For IE11. May we one day live without your BS.
                 Modernizr.addTest('preserve3d', function() {
@@ -36,84 +53,88 @@ include 'php/post_state.php';
                 });
         </script>
 
-        <main>
-                <div id="welcome" class="modal" tabindex="-1">
-                        <div class="modal-dialog">
-                                <div class="modal-content">
-                                        <div class="modal-header">
-                                                <h5 class="modal-title">m00dcards</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Tovább"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                                <p>Zárt felhasználású kártyajáték.</p>
-                                                <p>Kártya előnézethez/kiválaszsásához kattints egy kis kártyára.</p>
-                                                <p>A kiválasztott kártya felfordításához kattints a nagy kártyára.</p>
-                                                <p>A játék újrakezdéséhez kattints az 'Új játék' gombra.</p>
-                                                <p>A játék állapota az URL másolásával segítségével megosztható, menthető.</p>
-                                        </div>
-                                        <div class="modal-footer">
-                                                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
-                                        </div>
+
+        <nav class="navbar navbar-expand-sm navbar-dark fixed-top bg-dark mb-2">
+                <div class="container-fluid">
+                        <a class="navbar-brand" href="#">m00dcards</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarCollapse">
+                                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                                        <li class="nav-item">
+                                                <a class="nav-link active" aria-current="page" href="javascript:restartBtnClick()">Új játék</a>
+                                        </li>
+
+                                </ul>
+                        </div>
+                </div>
+        </nav>
+
+
+
+        <div id="welcome" class="modal" tabindex="-1">
+                <div class="modal-dialog">
+                        <div class="modal-content">
+                                <div class="modal-header">
+                                        <h5 class="modal-title">m00dcards</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Tovább"></button>
+                                </div>
+                                <div class="modal-body">
+                                        <p>Zárt felhasználású kártyajáték.</p>
+                                        <p>Kártya előnézethez/kiválaszsásához kattints egy kis kártyára.</p>
+                                        <p>A kiválasztott kártya felfordításához kattints a nagy kártyára.</p>
+                                        <p>A játék újrakezdéséhez kattints az 'Új játék' gombra.</p>
+                                        <p>A játék állapota az URL másolásával segítségével megosztható, menthető.</p>
+                                </div>
+                                <div class="modal-footer">
+                                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
                                 </div>
                         </div>
                 </div>
+        </div>
 
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                        <a class="navbar-brand" href="#">m00dcards</a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul class="navbar-nav mr-auto">
-                                        <li class="nav-item active">
-                                                <a id="btnRestart" class="nav-link" href="#">Új játék</span></a>
-                                        </li>
-                                </ul>
-                        </div>
-                </nav>
-
-                <div class="container">
-                        <div class="row mb-3">
-                                <div class="col-md-8 ">
-                                        <?php
-                                        $position = 0;
-                                        foreach ($init_state as $i) { ?>
-                                                <button id="sc<?php echo $position ?>" class="small-card" data-rel="img<?php printf('%02d', $i) ?>.jpg" data-index=<?php echo $i ?> data-position=<?php echo $position ?>>
-                                                        <div class="smallhover panel">
-                                                                <div class="front">
-                                                                        <div class="pad">
-                                                                                <img src="img/small/back/img<?php printf('%02d', $i) ?>.jpg" alt="?">
-                                                                        </div>
-                                                                </div>
-                                                                <div class="back">
-                                                                        <div class="pad">
-                                                                                <img src="img/small/front/img<?php printf('%02d', $i) ?>.jpg" alt="?" />
-                                                                        </div>
-                                                                </div>
-                                                        </div>
-
-                                                </button>
-                                        <?php $position += 1;
-                                        } ?>
-                                </div>
-                                <div class="col-md-4 ">
-                                        <div class="big-card" id="big-card">
-                                                <div class="bighover panel">
+        <main class="container">
+                <div class="row mb-3">
+                        <div class="col-md-8 ">
+                                <?php
+                                $position = 0;
+                                foreach ($init_state as $i) { ?>
+                                        <button id="sc<?php echo $position ?>" class="small-card" data-rel="img<?php printf('%02d', $i) ?>.jpg" data-index=<?php echo $i ?> data-position=<?php echo $position ?>>
+                                                <div class="smallhover panel">
                                                         <div class="front">
                                                                 <div class="pad">
-                                                                        <img src="img/MOODCARDS.jpg" alt="?">
+                                                                        <img src="img/small/back/img<?php printf('%02d', $i) ?>.jpg" alt="?">
                                                                 </div>
                                                         </div>
                                                         <div class="back">
-                                                                <div class="pad" id="big-card-back">
-                                                                        <img src="img/MOODCARDS.jpg" alt="?" />
+                                                                <div class="pad">
+                                                                        <img src="img/small/front/img<?php printf('%02d', $i) ?>.jpg" alt="?" />
                                                                 </div>
+                                                        </div>
+                                                </div>
+
+                                        </button>
+                                <?php $position += 1;
+                                } ?>
+                        </div>
+                        <div class="col-md-4 ">
+                                <div class="big-card" id="big-card">
+                                        <div class="bighover panel">
+                                                <div class="front">
+                                                        <div class="pad">
+                                                                <img src="img/MOODCARDS.jpg" alt="?">
+                                                        </div>
+                                                </div>
+                                                <div class="back">
+                                                        <div class="pad" id="big-card-back">
+                                                                <img src="img/MOODCARDS.jpg" alt="?" />
                                                         </div>
                                                 </div>
                                         </div>
                                 </div>
                         </div>
+                </div>
         </main>
 
         <script>
@@ -122,6 +143,7 @@ include 'php/post_state.php';
                         var imgUrl = $(small_card).data('rel');
                         var selectedIndex = $(small_card).data('index');
                         var selectedPosition = $(small_card).data('position');
+                        js_selected_index = selectedPosition;
                         $("#big-card").html('<div class="bighover panel"><div class="front"><div class="pad"><img src="img/big/back/' + imgUrl + '"></div></div><div class="back"><div class="pad" id="big-card-back"><img src="img/big/front/' + imgUrl + '"/></div></div></div>');
                         if (smallhover.classList.contains('flip')) {
                                 $('.bighover').addClass('flip');
@@ -129,7 +151,6 @@ include 'php/post_state.php';
                                 $('.bighover').click(function() {
                                         $(this).addClass('flip');
                                         $(smallhover).addClass('flip');
-                                        js_selected_index = selectedPosition;
                                         //post status to database
                                         $.ajax({
                                                 type: "POST",
@@ -158,27 +179,18 @@ include 'php/post_state.php';
 
                 }
 
-                var js_current_state = JSON.parse('<?php echo json_encode($current_state); ?>');
-                Array.from(js_current_state).forEach(element => {
-                        var sc = $('#sc' + element);
-                        if (sc) {
-                                var smallhover = sc.children('.smallhover');
-                                smallhover.addClass("flip");
-                        }
-                });
-                var js_selected_index = JSON.parse('<?php echo json_encode($selected_index); ?>');
-                if (js_selected_index != -1) {
-                        var sc = document.getElementById("sc" + js_selected_index);
-
-                        if (sc) {
-                                update_big_card(sc);
+                function restartBtnClick() {
+                        if (confirm("Újra szeretnéd kezdeni a játékot?")) {
+                                window.location.href = location.protocol + '//' + location.host + location.pathname;
                         }
                 }
+
 
                 $(".small-card").click(function() {
                         var small_card = this;
                         update_big_card(small_card);
                         var selectedPosition = $(small_card).data('position');
+                        js_selected_index = selectedPosition;
                         $.ajax({
                                 type: "POST",
                                 url: "php/post_state.php",
@@ -202,19 +214,38 @@ include 'php/post_state.php';
                                 }
                         });
                 });
+                
+                var js_current_state = null;
+                var js_selected_index = null;
+                var gameID=null;
+                var gameDbID = null;
 
                 $(document).ready(function() {
                         $('#welcome').modal('show');
                         $('.bighover').click(function() {
                                 $(this).addClass('flip');
                         });
-                        $('#btnRestart').click(function() {
-                                if (confirm("Újra szeretnéd kezdeni a játékot?")) {
-                                        window.location.href = location.protocol + '//' + location.host + location.pathname;
+
+                        js_current_state = JSON.parse('<?php echo json_encode($current_state); ?>');
+                        Array.from(js_current_state).forEach(element => {
+                                var sc = $('#sc' + element);
+                                if (sc) {
+                                        var smallhover = sc.children('.smallhover');
+                                        smallhover.addClass("flip");
                                 }
                         });
-                });
+                        js_selected_index = JSON.parse('<?php echo json_encode($selected_index); ?>');
+                        if (js_selected_index != -1) {
+                                var sc = document.getElementById("sc" + js_selected_index);
 
+                                if (sc) {
+                                        update_big_card(sc);
+                                }
+                        }
+                        gameID = '<?php echo $gameID; ?>';
+                        gameDbID = '<?php echo getDbGameID($gameID); ?>';
+
+                });
         </script>
 
 </body>
--- webpage/simple/php/post_state.php
+++ webpage/simple/php/post_state.php
@@ -1,17 +1,18 @@
 <?php
+
 use Medoo\Medoo;
+
 function merge_states($local_state, $remote_state, $selectedIndex)
 {
     $unstaged = array_diff($remote_state, $local_state);
     $state_counter = count($remote_state);
-    if($selectedIndex!=-1 && end($remote_state)!=$selectedIndex)
-    {
+    if ($selectedIndex != -1 && end($remote_state) != $selectedIndex) {
         $current_state = $remote_state;
         $current_state[$state_counter] = $selectedIndex;
-        $state_counter+=1;
-        return array("state_counter"=>$state_counter, "current_state"=>$current_state,"unstaged"=>$unstaged);
+        $state_counter += 1;
+        return array("state_counter" => $state_counter, "current_state" => $current_state, "unstaged" => $unstaged);
     }
-    return array("state_counter"=>$state_counter, "current_state"=>$remote_state,"unstaged"=>$unstaged);
+    return array("state_counter" => $state_counter, "current_state" => $remote_state, "unstaged" => $unstaged);
 }
 function post_new_state($selectedIndex)
 {
@@ -21,15 +22,23 @@ function post_new_state($selectedIndex)
     global $gameID;
     //update local state
     $db_state = get_last_state($gameID);
-    $merged = merge_states($current_state,$db_state["last_state"],$selectedIndex);
-    $unstaged = $merged["unstaged"];
-    if($merged["current_state"]!=$current_state);{
-        //db has unstaged remote change -> merge states, return unstaged changes
-        $current_state = $merged["current_state"];
-        $state_counter = $merged["state_counter"];
-        $database -> update("sessions",["selected"=>$selectedIndex,"last_state"=>json_encode($current_state),"ts"=>Medoo::raw('NOW()')],["game_id"=>getDbGameID($gameID)]);
+    if (count($db_state["last_state"]) == 0) {
+        $unstaged = array();
+        if($selectedIndex!=-1){
+            $current_state = array(0=>$selectedIndex);
+            $database->update("sessions", ["selected" => $selectedIndex, "last_state" => json_encode($current_state), "ts" => Medoo::raw('NOW()')], ["game_id" => getDbGameID($gameID)]);
+        }
+    } else {
+        $merged = merge_states($current_state, $db_state["last_state"], $selectedIndex);
+        $unstaged = $merged["unstaged"];
+        if ($merged["current_state"] != $current_state); {
+            //db has unstaged remote change -> merge states, return unstaged changes
+            $current_state = $merged["current_state"];
+            $state_counter = $merged["state_counter"];
+            $database->update("sessions", ["selected" => $selectedIndex, "last_state" => json_encode($current_state), "ts" => Medoo::raw('NOW()')], ["game_id" => getDbGameID($gameID)]);
+        }
     }
-    return array("unstaged" =>$unstaged, "current_state"=>$current_state);
+    return array("unstaged" => $unstaged, "current_state" => $current_state);
 }
 function post_selection($selectedIndex)
 {
@@ -39,18 +48,18 @@ function post_selection($selectedIndex)
     global $gameID;
     //update local state
     $db_state = get_last_state($gameID);
-    $merged = merge_states($current_state,$db_state["last_state"],$selectedIndex);
+    $merged = merge_states($current_state, $db_state["last_state"], $selectedIndex);
     $unstaged = $merged["unstaged"];
-    if($merged["current_state"]!=$current_state){
+    if ($merged["current_state"] != $current_state) {
         //db has unstaged remote change -> merge states, return unstaged changes
         $current_state = $merged["current_state"];
         $state_counter = $merged["state_counter"];
     }
-    if($selectedIndex!=-1) $database ->update("sessions",["selected"=>$selectedIndex,"ts"=>Medoo::raw('NOW()')],["game_id"=>getDbGameID($gameID)]);
-    return array("unstaged" =>$unstaged, "current_state"=>$current_state);
+    if ($selectedIndex != -1) $database->update("sessions", ["selected" => $selectedIndex, "ts" => Medoo::raw('NOW()')], ["game_id" => getDbGameID($gameID)]);
+    return array("unstaged" => $unstaged, "current_state" => $current_state);
 }
 if (isset($_POST['new_state']) && isset($_POST['gameID']) && isset($_POST['current_state'])) {
-    if(!function_exists("get_last_state")) require_once 'ajax_session.php';
+    if (!function_exists("get_last_state")) require_once 'ajax_session.php';
     $gameID = $_POST['gameID'];
     $current_state = json_decode($_POST['current_state']);
     $selectedIndex = intval($_POST['new_state']);
@@ -60,7 +69,7 @@ if (isset($_POST['new_state']) && isset($_POST['gameID']) && isset($_POST['curre
 }
 
 if (isset($_POST['selection_changed'])  && isset($_POST['gameID']) && isset($_POST['current_state'])) {
-    if(!function_exists("get_last_state")) require_once 'ajax_session.php';
+    if (!function_exists("get_last_state")) require_once 'ajax_session.php';
     $gameID = $_POST['gameID'];
     $current_state = json_decode($_POST['current_state']);
     $selectedIndex = intval($_POST['selection_changed']);
